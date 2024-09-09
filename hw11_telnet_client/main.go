ec133584bd859b1ffd02b0def2d39b5a8c1973d1
Roman Kabaev <r.kabaev@yadro.com>
2024-09-09 21:43:17 +0300

    hw11 done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,52 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "Timeout for connection in seconds")
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		fmt.Fprintln(os.Stderr, "...Required args are not provided")
+	}
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "...Could not establish connection")
+		return
+	}
+
+	go func() {
+		for {
+			err := client.Receive()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "...Connection closed by peer")
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			err := client.Send()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "...Connection closed by peer")
+				return
+			}
+		}
+	}()
+
+	<-client.Done()
+	client.Close()
 }
